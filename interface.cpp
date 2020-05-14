4673b752dfd2c4772bb94b29963676281f7002b4
yurifisica <yuriferreira.fis@gmail.com>
2020-05-13 22:54:25 -0300

    interface finalizada (exceto acentuação)

--- interface.cpp
+++ interface.cpp
@@ -1,16 +1,9 @@
 /****************************************************************************
-
-  example5.cpp
-
-  A GLUI program demonstrating subwindows, rotation controls,
-	translation controls, and listboxes
-
-  -----------------------------------------------------------------------
-
-  7/10/98 Paul Rademacher (rademach@cs.unc.edu)
-
+Programa de simulação computacional utilizando a tecnica de elementos finitos
+Autor: Yuri Ferreira dos Santos
 ****************************************************************************/
 #include <math.h>
+#include <locale>
 #ifdef __APPLE__
 #ifndef GL_SILENCE_DEPRECATION
 #define GL_SILENCE_DEPRECATION
@@ -30,55 +23,120 @@
 #else
 #include <GL/glut.h>
 #endif
-
+#define ENABLED_PRIM 101
+#define ENABLED_SEC 201
+#define DISABLED 100
+#define CONFIRMED 111
+#define CLEAR 0
 /** Pointers to the windows and some of the controls we'll create **/
-GLUI *glui, *glui2;
+GLUI *glui, *glui_prim,*glui_sec,*glui_alert;
 GLUI_Spinner    *light0_spinner, *light1_spinner;
 GLUI_RadioGroup *radio;
-GLUI_Panel      *obj_panel;
+GLUI_Panel      *obj_panel,*medium_panel_prim,*medium_panel_sec;
+GLUI_EditText *condutivity;
 int main_window;
 
 char texto[30];
 GLfloat win, r, g, b, xf,zf;
 GLfloat coord[10][2];
+float tw1,th1;
 GLint view_w, view_h, primitiva;
 int nx=50,nz=40,np;
 float x0=0;
 float xn=100;
 float z0=0;
 float zn=80;
+float sigma,sigma_aux,sigma_sec[30];
 double dx,dz;
-double *x,*z,*xv,*zv;
+double *x,*z,*xv,*zv,*xp,*zp;
 double xvp,zvp;//coordenada do vertice mais proximo ao clique do usuario
 double dist,aux_dist;
 int *click_xv, *click_zv;
-int cont,ic,i,j;
-double *polygon[2];
-int prim,click=0;
+int cont,ic,i,j,qt_prim,qt_sec;
+// float medium_prim[10][2][2];
+float medium_sec[30][2][2];
+int sec,prim,click=0;
 int menu;
 int tx, ty, tw, th;
 void myGlutIdle()
 {
   /* According to the GLUT specification, the current window is
-     undefined during an idle callback.  So we need to explicitly change
-     it if necessary */
+  undefined during an idle callback.  So we need to explicitly change
+  it if necessary */
   if ( glutGetWindow() != main_window )
-    glutSetWindow(main_window);
+  glutSetWindow(main_window);
 
   /*  GLUI_Master.sync_live_all();  -- not needed - nothing to sync in this
-                                       application  */
+  application  */
 
   glutPostRedisplay();
 }
+
+void control_cb(int control)
+{
+  // if (control==ENABLED_PRIM)
+  // {
+  //   prim=1;
+  //   sec=0;
+  //   click=0;
+  //   glui_prim->show();
+  // }
+  if (control==ENABLED_SEC)
+  {
+    //prim=0;
+    sec=1;
+    click=0;
+    glui_sec->show();
+    // glui_sec->enable();
+  }
+  if (control==DISABLED)
+  {
+    prim=0;
+    sec=0;
+    click=0;
+    glui_prim->hide();
+    glui_sec->hide();
+  }
+  if (control==CONFIRMED)
+  {
+    // if (prim==1)
+    // {
+    //   qt_prim++;
+    //   glui_prim->hide();
+    // }else
+    if (sec==1) {
+      sigma_sec[qt_sec]=sigma_aux;
+      qt_sec++;
+      glui_sec->hide();
+    }
+  }
+  if (control==CLEAR)
+  {
+    qt_prim=0;
+    qt_sec=0;
+    prim=0;
+    sec=0;
+    click=0;
+    glutPostRedisplay();
+    for (i=0;i<np;i++)
+    {
+      click_xv[i]=0;
+      click_zv[i]=0;
+    }
+  }
+}
+
 void DesenhaPontos(void)
 {
   np=nx*nz;
-  dx=(xn-x0)/(nx+1);
-  dz=(zn-z0)/(nz+1);
+  dx=abs(xn-x0)/(nx-1);
+  dz=abs(zn-z0)/(nz-1);
   x=(double*)malloc(nx*sizeof(double));
   z=(double*)malloc(nz*sizeof(double));
   xv=(double*)malloc(np*sizeof(double));
   zv=(double*)malloc(np*sizeof(double));
+  xp=(double*)malloc(np*sizeof(double));
+  zp=(double*)malloc(np*sizeof(double));
   click_xv=(int*)malloc(np*sizeof(int));
   click_zv=(int*)malloc(np*sizeof(int));
   i=0;
@@ -93,26 +151,21 @@ void DesenhaPontos(void)
       i++;
     }
   }
-  i=0;
-  float tw1=tw-xn,th1=th-zn;
-  float lixo=tw1/2;//(x[nx-1]-x[0])*tw1/200;
-  float lixo2=th1/2;//(z[nz-1]-z[0])*tw1/160;
+  tw1=tw-xn;
+  th1=th-zn;
+  for (i=0;i<np;i++)
+  {
+    xp[i]=xv[i]*tw1/xn;
+    zp[i]=zv[i]*th1/zn;
+  }
   glPointSize(3);
   glBegin(GL_POINTS);
   for (cont=0;cont<np;cont++)
   {
     for (ic=0;ic<np;ic++)
     {
-      if (click_xv[cont]==1 && click_zv[ic]==1)
-      {
-          glColor3f(1,0,0);
-      }
-      else
-      {
-          glColor3f(0,0,0);
-      }
-        glVertex2f((xv[cont])*tw1/x[nx-1]-lixo,zv[ic]*th1/z[nz-1]-lixo2);
-        // printf("%f %f\n",xv[cont]-tw1/2,zv[ic]+th1/2);
+      glColor3f(0,0,0);
+      glVertex2f((xp[cont])-tw1/2,zp[ic]-th1/2);
     }
   }
   glEnd();
@@ -121,8 +174,8 @@ void DesenhaPontos(void)
   {
     for (ic=0;ic<np;ic++)
     {
-        glVertex2f((xv[cont])*tw1/x[nx-1]-lixo,zv[ic]*th1/z[nz-1]-lixo2);
-        glVertex2f((xv[cont+1])*tw1/x[nx-1]-lixo,zv[ic]*th1/z[nz-1]-lixo2);
+      glVertex2f((xp[cont])-tw1/2,zp[ic]-th1/2);
+      glVertex2f((xp[cont+1])-tw1/2,zp[ic]-th1/2);
     }
   }
   glEnd();
@@ -131,63 +184,63 @@ void DesenhaPontos(void)
   {
     for (ic=0;ic<np;ic++)
     {
-      glVertex2f((xv[cont])*tw1/x[nx-1]-lixo,zv[ic]*th1/z[nz-1]-lixo2);
-      glVertex2f((xv[cont])*tw1/x[nx-1]-lixo,zv[ic+1]*th1/z[nz-1]-lixo2);
-        // printf("%f %f\n",xv[cont]-tw/2,zv[ic]+th/2);
+      glVertex2f((xp[cont])-tw1/2,zp[ic]-th1/2);
+      glVertex2f((xp[cont])-tw1/2,zp[ic+1]-th1/2);
     }
   }
   glEnd();
 }
 // Desenha um texto na janela GLUT
-void DesenhaTexto(char *string)
+void DesenhaTexto(char *string, int x, int z)
 {
   glPushMatrix();
   // Posição no universo onde o texto será colocado
-  glRasterPos2f(-win,win-2);
+  glRasterPos2f(x,z);
   glColor3f(1,0,0);
   // Exibe caracter a caracter
   while(*string)
-  glutBitmapCharacter(GLUT_BITMAP_9_BY_15,*string++);
+  glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10,*string++);
   glPopMatrix();
 }
 //desenha Meios
 void DesenhaMeio(void)
 {
-  glColor3f(1,0,0);
-  if (prim==1)
-  {
-    printf("%d\n",click );
-      if (click>1)
-      {
-        for (ic=2;ic<=click;ic++)
-        {
-          printf("%f\n",polygon[0][ic-2]);
-          printf("%f\n",polygon[0][ic-1]);
-        }
-        glBegin(GL_LINES);
-        glVertex2f(polygon[0][click-2],polygon[1][click-2]);
-        glVertex2f(polygon[0][click-1],polygon[1][click-1]);
-        glEnd();
-      }
-      glBegin(GL_LINE_STRIP);
-        for (cont=0;cont<click;cont++)
-        {
-          glVertex2f(polygon[0][cont],polygon[1][cont]);
-        }
-      glEnd();
-      // if (polygon[0][click-1]==polygon[0][0] && polygon[1][click-1]==polygon[1][0])
+    if (qt_sec!=0)
+    {
+      // glColor3f(1,0,0);
+      // glBegin(GL_QUADS);
+      // for (i=0;i<qt_prim;i++)
+      // {
+      //   if ((i<qt_prim-1) | (click==2) | (sec==1))
       //   {
-      //     glBegin(GL_POLYGON);
-      //     for (cont=0;cont<click;cont++)
-      //     {
-      //       glVertex2f(polygon[0][cont],polygon[1][cont]);
-      //     }
-      //     glEnd();
-      //     prim=0;
+      //     glVertex2f(medium_prim[i][0][0]-tw1/2,-medium_prim[i][0][1]+th1/2);
+      //     glVertex2f(medium_prim[i][1][0]-tw1/2,-medium_prim[i][0][1]+th1/2);
+      //     glVertex2f(medium_prim[i][1][0]-tw1/2,-medium_prim[i][1][1]+th1/2);
+      //     glVertex2f(medium_prim[i][0][0]-tw1/2,-medium_prim[i][1][1]+th1/2);
       //   }
+      // }
+      // glEnd();
+      // printf("%d\n",qt_sec );
+      for (i=0;i<qt_sec;i++)
+      {
+        if ((i<qt_sec-1) | (click==2))
+        {
+        glColor3f(0,1,0);
+        glBegin(GL_QUADS);
+          glVertex2f(medium_sec[i][0][0]-tw1/2,-medium_sec[i][0][1]+th1/2);
+          glVertex2f(medium_sec[i][1][0]-tw1/2,-medium_sec[i][0][1]+th1/2);
+          glVertex2f(medium_sec[i][1][0]-tw1/2,-medium_sec[i][1][1]+th1/2);
+          glVertex2f(medium_sec[i][0][0]-tw1/2,-medium_sec[i][1][1]+th1/2);
+        glEnd();
+          sprintf(texto,"%.4f",sigma_sec[i]);
+          // printf("%d -> %f\n",i,sigma_sec[i]);
+          glColor3f(1,0,0);
+          DesenhaTexto(texto,(medium_sec[i][1][0]+medium_sec[i][0][0])/2-tw1/2,(-medium_sec[i][1][1]-medium_sec[i][0][1])/2+th1/2);
+        }
+      }
+      }
+    // glutPostRedisplay();
   }
-}
-
 // Função callback chamada para fazer o desenho
 void Desenha(void)
 {
@@ -196,10 +249,11 @@ void Desenha(void)
   glClear(GL_COLOR_BUFFER_BIT);
   DesenhaMeio();
   DesenhaPontos();
-  DesenhaTexto(texto);
+  // DesenhaTexto(texto,0,0);
   // Exibe a posição do mouse na janela
   glColor3f(1.0f,1.0f,1.0f);
   glutSwapBuffers();
+  // glutPostRedisplay();
 }
 void Inicializa (void)
 {
@@ -219,7 +273,7 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
   // Especifica as dimensões da Viewport
   GLUI_Master.get_viewport_area ( &tx, &ty, &tw, &th );
   glViewport( tx, ty, tw, th );
-  printf("%i %i\n",tw, th );
+  // printf("%i %i\n",tw, th );
   view_w = w;
   view_h = h;
   // Inicializa o sistema de coordenadas
@@ -228,78 +282,72 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
   gluOrtho2D (-tw/2, tw/2, -th/2, th/2);
 }
 
-// Função callback chamada para gerenciar eventos do teclado
-// para teclas especiais, tais como F1, PgDn e Home
-void TeclasEspeciais(int key, int x, int y)
+void GerenciaMouse(int button, int state, int x_m, int z_m)
 {
-  if(key == GLUT_KEY_UP) {
-    win -= 10;
-    if (win < 10) win = 10;
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D (-win, win, -win, win);
-  }
-  if(key == GLUT_KEY_DOWN) {
-    win += 10;
-    if (win > 500) win = 500;
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D (-win, win, -win, win);
-  }
-  glutPostRedisplay();
-}
-
-void GerenciaMouse(int button, int state, int x, int z)
-{
-if (button==GLUT_LEFT_BUTTON)
+  if (button==GLUT_LEFT_BUTTON)
   {
     if (state==GLUT_DOWN)
     {
+      // if (prim==1)
+      // {
+      //   if (click<2)
+      //   {
+      //     aux_dist=10000;
+      //     for (i=0;i<np;i++)
+      //     {
+      //       dist=sqrt(pow(x_m-xp[i]-xn/2,2)+pow(z_m-zp[i]-zn/2,2));
+      //       if (dist<aux_dist)
+      //       {
+      //         xvp=xp[i];
+      //         zvp=zp[i];
+      //         click_xv[i]=1;
+      //         click_zv[i]=1;
+      //         aux_dist=dist;
+      //       }
+      //     }
+      //     medium_prim[qt_prim-1][click][0]=xvp;
+      //     medium_prim[qt_prim-1][click][1]=zvp;
+      //     click++;
+      //   }
+      // }
+      if (sec==1)
+      {
         if (click<2)
         {
-          sprintf(texto,"Digite coordenada %i",click+1);
-          coord[click][0]=x;
-          coord[click][1]=z;
-          printf("%f , %f\n",coord[click][0],coord[click][1]);
           aux_dist=10000;
           for (i=0;i<np;i++)
           {
-            printf("%i-> %f , %f\n",i, xv[i], zv[i]);
-            dist=sqrt(pow(coord[click][0]-xv[i],2)+pow(coord[click][1]-zv[i],2));
+            dist=sqrt(pow(x_m-xp[i]-xn/2,2)+pow(z_m-zp[i]-zn/2,2));
             if (dist<aux_dist)
-              {
-                xf=xv[i];
-                zf=zv[i];
-                aux_dist=dist;
-              }
+            {
+              xvp=xp[i];
+              zvp=zp[i];
+              click_xv[i]=1;
+              click_zv[i]=1;
+              aux_dist=dist;
             }
-          printf("%f , %f\n", xf, zf );
-          printf("%f , %f\n",coord[click][0],coord[click][1]);
-           // printf("%f %f\n",xn/2,zn/2 );
+          }
+          medium_sec[qt_sec-1][click][0]=xvp;
+          medium_sec[qt_sec-1][click][1]=zvp;
           click++;
         }
-      // if (prim==1)
-      // {
-      //   xf = ( (2 * win * x) / view_w) - win;
-      //   zf = ( ( (2 * win) * (z-view_h) ) / -view_h) - win;
-      //       polygon[0][click]=xf;
-      //       polygon[1][click]=zf;
-      //       click++;
-      // }
+      }
     }
   }
-
   glutPostRedisplay();
 }
-void MoveMouse(int x, int z)
+void MoveMouse(int x_m, int z_m)
 {
-        sprintf(texto, "(%d , %d)", x, z);
-        glutPostRedisplay();
+  sprintf(texto, "(%d , %d)", x_m, z_m);
+  glutPostRedisplay();
 }
 /**************************************** main() ********************/
 
 int main(int argc, char* argv[])
 {
+  setlocale(LC_ALL, "");
+  qt_prim=0;
+  qt_sec=0;
   /****************************************/
   /*   Initialize GLUT and create window  */
   /****************************************/
@@ -307,19 +355,18 @@ int main(int argc, char* argv[])
   glutInit(&argc, argv);
   glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );
   glutInitWindowPosition( 50, 50 );
-  glutInitWindowSize( 1000, 700 );
-  main_window = glutCreateWindow( "GLUI Example 5" );
+  glutInitWindowSize( 1100, 700 );
+  main_window = glutCreateWindow( "Elementos Finitos - ondas planas" );
   GLUI_Master.set_glutReshapeFunc( AlteraTamanhoJanela);
+  GLUI_Master.set_glutMouseFunc( GerenciaMouse );
   glutDisplayFunc( Desenha );
   //GLUI_Master.set_glutSpecialFunc( TeclasEspeciais);
-  GLUI_Master.set_glutMouseFunc( GerenciaMouse );
-  glutPassiveMotionFunc(MoveMouse);
+  // glutPassiveMotionFunc(MoveMouse);
   printf("GLUI version: %3.2f\n", GLUI_Master.get_version());
 
   /*** Create the side subwindow ***/
   glui = GLUI_Master.create_glui_subwindow( main_window,GLUI_SUBWINDOW_RIGHT );
-
-  obj_panel = new GLUI_Rollout(glui, "Properties", true );
+  obj_panel = new GLUI_Rollout(glui, "Propriedades da malha", true );
   GLUI_Spinner *x0_spinner = new GLUI_Spinner(obj_panel,"xi",&x0);
   x0_spinner->set_alignment( GLUI_ALIGN_RIGHT );
   x0_spinner->set_float_limits(0,100);
@@ -329,28 +376,56 @@ int main(int argc, char* argv[])
   GLUI_Spinner *z0_spinner = new GLUI_Spinner(obj_panel,"zi",&z0);
   z0_spinner->set_float_limits(0,100);
   z0_spinner->set_alignment( GLUI_ALIGN_RIGHT );
-GLUI_Spinner *zn_spinner = new GLUI_Spinner(obj_panel,"zn",&zn);
-zn_spinner->set_alignment( GLUI_ALIGN_RIGHT );
-zn_spinner->set_float_limits(0,100);
+  GLUI_Spinner *zn_spinner = new GLUI_Spinner(obj_panel,"zn",&zn);
+  zn_spinner->set_alignment( GLUI_ALIGN_RIGHT );
+  zn_spinner->set_float_limits(0,100);
   GLUI_Spinner *nx_spinner = new GLUI_Spinner(obj_panel,"nx",&nx);
   nx_spinner->set_int_limits(10,50);
   nx_spinner->set_alignment( GLUI_ALIGN_RIGHT );
-GLUI_Spinner *nz_spinner = new GLUI_Spinner(obj_panel,"nz",&nz);
-nz_spinner->set_alignment( GLUI_ALIGN_RIGHT );
-nz_spinner->set_float_limits(10,50);
+  GLUI_Spinner *nz_spinner = new GLUI_Spinner(obj_panel,"nz",&nz);
+  nz_spinner->set_alignment( GLUI_ALIGN_RIGHT );
+  nz_spinner->set_float_limits(10,50);
   new GLUI_StaticText( glui, "" );
-
+  medium_panel_prim = new GLUI_Panel(glui,"Meio primario");
+  condutivity = new GLUI_EditText(medium_panel_prim,"Condutividade (siemens/m): ",&sigma);
+  new GLUI_StaticText( glui, "" );
+  medium_panel_sec =  new GLUI_Panel(glui,"Meio Secundario");
+  new GLUI_Button(medium_panel_sec,"Adicionar",ENABLED_SEC,control_cb);
+  new GLUI_Button(medium_panel_sec,"Limpar Malha",CLEAR,control_cb);
+  new GLUI_StaticText(glui,"");
   /****** A 'quit' button *****/
   new GLUI_Button( glui, "Quit", 0,(GLUI_Update_CB)exit );
+  new GLUI_StaticText(glui,"");
+  new GLUI_StaticText(glui,"Instrução:");
+  new GLUI_StaticText(glui,"");
+  new GLUI_StaticText(glui,"Para desenhar o meio secundario, clique");
+  new GLUI_StaticText(glui,"no no correspondente ao canto inferior");
+  new GLUI_StaticText(glui,"esquerdo e apos no no´ correspondente");
+  new GLUI_StaticText(glui,"ao canto superior  direito do retangulo");
+  new GLUI_StaticText(glui,"desejado ");
 
+  // glui_prim = GLUI_Master.create_glui("Meio primario");
+  // condutivity = new GLUI_EditText(glui_prim,"Condutividade: ",&sigma);
+  // GLUI_Panel *panel1 = new GLUI_Panel(glui_prim,"",false);
+  // new GLUI_Button(panel1,"Confirmar",CONFIRMED,control_cb);
+  // new GLUI_Column(panel1,false);
+  // new GLUI_Button(panel1,"Cancelar",DISABLED,control_cb);
+  // glui_prim->hide();
+  glui_sec = GLUI_Master.create_glui("Meio secundario");
+  condutivity = new GLUI_EditText(glui_sec,"Condutividade(siemens/m): ",&sigma_aux);
+  GLUI_Panel *panel2 = new GLUI_Panel(glui_sec,"",false);
+  new GLUI_Button(panel2,"Confirmar",CONFIRMED,control_cb);
+  new GLUI_Column(panel2,false);
+  new GLUI_Button(panel2,"Cancelar",DISABLED,control_cb);
+  glui_sec->hide();
 
   /**** Link windows to GLUI, and register idle callback ******/
 
   glui->set_main_gfx_window( main_window );
-#if 0
+  #if 0
   /**** We register the idle callback with GLUI, *not* with GLUT ****/
   GLUI_Master.set_glutIdleFunc( myGlutIdle );
-#endif
+  #endif
 
   /**** Regular GLUT main loop ****/
   Inicializa();
